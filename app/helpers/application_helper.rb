9f68d37be1cd8b1fb489e99f3cce9a1ebeba82f9
LucyWeltner <lucyiweltner@gmail.com>
2020-02-13 11:29:18 -0500

    Moves forms to read books and add books to lists into partials.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
 	def is_logged_in?
 		!!session[:user_id]
 	end
-	def read?(book_id)
-		found = ReadBook.find_by(book_id: book_id, user_id: session[:user_id])
-		if found 
-			return true
-		else 
-			return false 
-		end 
-	end
+	# def read?(book_id)
+	# 	found = ReadBook.find_by(book_id: book_id, user_id: session[:user_id])
+	# 	if found 
+	# 		return true
+	# 	else 
+	# 		return false 
+	# 	end 
+	# end
 
 end
