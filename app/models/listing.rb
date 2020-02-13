5a3c6cf1dbc2ab43fe8f30508c4c48c92483895f
LucyWeltner <lucyiweltner@gmail.com>
2020-02-13 10:58:07 -0500

    Comments about specific list items now appear in list show pages.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
 	belongs_to :book
 	belongs_to :list
+	def self.find_listing(book: book, list: list)
+		self.where(book_id: book.id).where(list_id: list.id)[0]
+	end
 end
